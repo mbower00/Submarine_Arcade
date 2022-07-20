b63130a4b2aed3f1411c2dd60727f77e28cf8bbb
Mitchell Bower <mitchell@bowermail.net>
2022-07-20 17:11:14 -0600

    ip

--- Constants.py
+++ Constants.py
@@ -3,10 +3,10 @@ SERVER_PORT = 1500
 CLIENT_PORT = 1501
 
 # IP for the black computer
-BLACK_IP = "192.168.2.230"
+BLACK_IP = "192.168.29.32"
 
 # IP for the white computer
-WHITE_IP = "192.168.6.116"
+WHITE_IP = "192.168.29.34"
 # WHITE_IP = "192.168.2.230"
 
 SCREEN_WIDTH = 600
